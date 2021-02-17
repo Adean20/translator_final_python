c99aa1037fdf35203b0caefd419e5f585a86068e
Austin Dean <austindean3@hotmail.com>
2021-02-17 17:52:04 +0000

    added authenticator and started function

---
+++ translator.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+#Authenticate api
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAutenticator
+
+API_KEY = os.environ.get("API_KEY")
+API_URL = os.environ.get("API_URL")
+
+authenticator = IAMAuthenticator(API_KEY)
+language_translator = LanguageTranslatorV3(
+    version= "2018-05-01",
+    authenticator = authenticator
+)
+
+language_translator.set_service_url(API_URL)
+
+
+def englishToFrench():
+    print("Please enter English text to be translated to French.")
+    initial_text = input()
+
+    result = language_translator.translate(
+        text = initial_text,
+        model_id = 'en-fr'
+    ).get_result()
+#fix this
+    print(result)
+    return result
